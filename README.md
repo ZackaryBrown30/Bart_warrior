611e09030a08e5c9cebe455a409df0214ec6a35c
ZackaryBrown30 <zackarybrown30@gmail.com>
2020-02-06 17:33:00 -0800

    Added Accordion file for Location updating

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-In order to make a clone of this app work you will need to add a file called key.js in your root directory and export a variable key: "Your-Key-Here", bart supplies a free public key that is prone to changing from time to time, or you may request a legacy key for your peronal use that will not in theory expire.
+Bart-App
+NodeJS/EJS/Express Bart-Api; Template rendering application using OOP, restful routing, and MVC standards.
+
+Intended to be the lightest possible application for easy access in low connectivity zones. Built with Mobile users in mind, chrome/broswer native.
+
+In order to make a clone of this app, you will need to add a file called key.js in your App's root directory and export a variable key: "Your-Key-Here", bart supplies a free public key that is prone to changing from time to time, or you may request a legacy key for your peronal use that will not in theory expire.
+
+See server.js, in the Api calls the secure key was imported and supplies access.
--- station .js
+++ station .js
@@ -1,16 +1,13 @@
 module.exports = {
-    12: "12th",
-    16: "16th",
-    19: "19th",
-    24:	"24th",
-    Ashby: "ashb",
-    Antioch: "antc",
-    BalboaPark: "balb",
-    BayFair : "bayf",
-   /* bery:	Berryessa
+  16: "16th",
+  24: "24th",
+  Ashby: "ashb",
+  Antioch: "antc",
+  BalboaPark: "balb",
+  BayFair: "bayf"
+  /* bery:	Berryessa
     cast	Castro Valley
     civc	Civic Center (SF)
-    cols	Coliseum
     colm	Colma
     conc	Concord
     daly	Daly City
@@ -20,25 +17,25 @@ module.exports = {
     plza	El Cerrito Plaza
     embr	Embarcadero (SF)
     frmt	Fremont
-    ftvl	Fruitvale (Oakland)
+
     glen	Glen Park (SF)
     hayw	Hayward
     lafy	Lafayette
-    lake	Lake Merritt (Oakland)
-    mcar	MacArthur (Oakland)
+
+
     mlbr	Millbrae
     mlpt	Milpitas
     mont	Montgomery St. (SF)
     nbrk	North Berkeley
     ncon	North Concord/Martinez
-    oakl	Oakland Int'l Airport
+   
     orin	Orinda
     pitt	Pittsburg/Bay Point
     pctr	Pittsburg Center
     phil	Pleasant Hill
     powl	Powell St. (SF)
     rich	Richmond
-    rock	Rockridge (Oakland)
+    rock	Rockridge (Oakland) / Berkley
     sbrn	San Bruno
     sfia	San Francisco Int'l Airport
     sanl	San Leandro
@@ -49,4 +46,4 @@ module.exports = {
     wcrk	Walnut Creek
     wdub	West Dublin
     woak	West Oakland   */
-    }
+};
