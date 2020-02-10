4943fa5790eb2ff52fb00ba080c38b0066badf04
ZackaryBrown30 <zackarybrown30@gmail.com>
2020-02-10 11:04:49 -0800

    refactored server.js

---
+++ secure/publicKey.js
@@ -0,0 +1,5 @@
+module.exports = {
+    tempKey: "MW9S-E7SL-26DU-VV8V",
+  };
+
+  /* Public Api Key for quick and easy access for guest users. It is encouraged to attain a unique key from bart if you are building your own app, see README */
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require("express");
 const flash = require("express-flash");
 const request = require("request");
 const app = express();
-const Key = require("./key");
+const Key = require("./secure/key");
+const PublicKey = require("./secure/publicKey")
 app.use(express.static("client/static"));
 app.use(bodyParser.json());
 app.use(express.json());
-
 app.use(
   bodyParser.urlencoded({
     extended: false
@@ -17,53 +17,35 @@ app.use(
 
 var Mykey = Key.key;
 var cmd = "etd";
-var Tempkey = Key.tempKey;
+var Tempkey = PublicKey.tempKey;
 var destination = "19th";
 
 
 app.get("/", (req, res) => {
-  request(
-    "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" +
-      destination +
-      "&dir=n&key=" +
-      Mykey +
-      "&json=y",
-      (error, response, north) => {   
-      console.error("North error:", error);
-      request(
-        "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" +destination+ "&dir=s&key=" +Mykey+ "&json=y",
-       (error, response, south) => {   
-        console.error("South error:", error);
-        res.render("index.ejs", {
-          South: JSON.parse(south),
-          North: JSON.parse(north)    
-          }); 
-        }
-      );
-    }
-  );
-});
+  Bi_directionalApiCall(res)});
 
-
-app.get("/:station", (req, res, next) => {
+app.get("/:station", (req, res) => {
   (destination = req.params.station),
+  Bi_directionalApiCall(res)});
+
+  /* Calls the bart api for data with parameters that supply northbound, then southbound trains */
+Bi_directionalApiCall = (res)  => { 
+  request(
+  "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" + destination + "&dir=n&key=" + Tempkey + "&json=y",
+    (error, response, north) => {   
+    console.error("North error:", error);
     request(
-          "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" +destination+ "&dir=n&key=" +Mykey+ "&json=y",
-      (error, response, north) => {
-        console.error("North error:", error);
-        request(
-          "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" +destination+ "&dir=s&key=" +Mykey+ "&json=y",
-          (error, response, south) => {
-            console.error("South error:", error);
-            res.render("index.ejs", {
-              South: JSON.parse(south),
-              North: JSON.parse(north)
-            });
-          }
-        );
+      "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=" +destination+ "&dir=s&key=" +Tempkey+ "&json=y",
+      (error, response, south) => {   
+      console.error("South error:", error);
+      res.render("index.ejs", {
+        South: JSON.parse(south),
+        North: JSON.parse(north)    
+        }); 
       }
     );
-});
+  })};
+
 
 const server = app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
